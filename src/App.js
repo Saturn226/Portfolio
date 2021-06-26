5d4f0896d614634578d1fb50068b5bf6b3260f81
Saturn226 <serenehopes@gmail.com>
2021-06-26 17:15:41 -0400

    added code to scroll to top on page navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import HeaderContainer from './containers/header';
-import { Home } from './pages';
+import { Home, About, Blog } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './helpers/scrollToTop';
 
 export function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route exact path='/'>
           <Home />
+        </Route>
+
+        <Route path='/about'>
+          <About />
+        </Route>
+
+        <Route path='/blog'>
+          <Blog />
         </Route>
       </Switch>
     </Router>
